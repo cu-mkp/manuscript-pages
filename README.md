ff27c82076bb68338349591cf9eeeff7416c0484
emretetik96 <et2406@columbia.edu>
2016-06-08 10:05:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+source venv/bin/activate
+
+Set up virtual environment
+
+Get private key from google developer console
+
+In the directory from which you run the program, create a folder titled "manuscript_downloads"
+
+To run: python server.py
---
+++ server.py
@@ -0,0 +1,129 @@
+from __future__ import print_function
+import httplib2
+
+import os
+import io
+import json
+import urllib
+
+from apiclient import discovery
+from apiclient import http
+from apiclient import errors
+
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+import re
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('./')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'drive-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def download_file_by_url(url, path):
+    try:
+        testfile = urllib.URLopener()   
+        testfile.retrieve(url, path)
+    except:
+        return
+
+#given a file title, return the file's new title, according to the naming convention:
+# tc_p057r ==> [057]_[tc]_preTEI.xml
+def get_new_file_title(old_title):
+    m = re.search('\d+[rv]', old_title)
+    folio_number = m.group(0)
+    m = re.search('[tcnl]+', old_title)
+    #file_type refers to whether it is tc, tcn, or tl
+    file_type = m.group(0)
+    new_file_title = folio_number + "_" + file_type + "_preTEI.xml"
+    return new_file_title
+
+def main():
+    """Shows basic usage of the Google Drive API.
+
+    Creates a Google Drive API service object and outputs the names and IDs
+    for up to 10 files.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v2', http=http)
+
+    #Get each folder in manuscript pages
+    folders = service.files().list(q="'0B42QaQPHLJloNnZhakpiVk9GRmM' in parents").execute()
+    folders_hash = folders["items"]
+
+    for folder in folders_hash:
+        #get the folder's id
+        try:
+            folder_id = folder["id"]
+            print(folder_id)
+            print(folder["title"])
+        except:
+            print("no title")
+
+        #using the folder's id, grab all files within the folder
+        files_within_folder = service.files().list(q="'" + folder_id + "' in parents").execute()
+        files = files_within_folder.get('items', [])
+        if not files:
+            print('No files found.')
+        else:
+            print('Files:')
+            #grab the export link (if it exists) of each file within the folder
+            for f in files: 
+                try:
+                    #grab the file's title and generate the new title
+                    ftitle = f["title"]
+                    new_file_title = get_new_file_title(ftitle)
+                    print(ftitle)
+                    print(new_file_title)
+                    #grab the file's exportLink to download it
+                    #fkey, flink = f["exportLinks"].get("text/plain", "failed")#.popitem()
+                    flink = f["exportLinks"]["text/plain"]
+                    print(flink)
+                    #using exportLink, download and save the file with its new title
+                    download_file_by_url(flink, "manuscript_downloads/" + new_file_title)
+                except:
+                    #print("============================")
+                    print("No exportLink for this file")
+                    #print("============================")
+    print(len(folders_hash))
+
+if __name__ == '__main__':
+    main()
