0347aee9c04cda90469bc0c5e67d646eb931c587
Philip Cherian <philip@dyn-129-236-212-108.dyn.columbia.edu>
2016-06-15 11:48:24 -0400

    Progress on querying for single file

--- server.py
+++ server.py
@@ -108,7 +108,9 @@ def main():
         os.makedirs("./manuscript_downloads/" + str(x).zfill(3) + "v")
 
     #Get each folder in manuscript pages
-    folders = service.files().list(q="'0B42QaQPHLJloNnZhakpiVk9GRmM' in parents", maxResults="200").execute()
+    #folders = service.files().list(q="'0B42QaQPHLJloNnZhakpiVk9GRmM' in parents", maxResults="200").execute()
+    folders = service.files().list(q="title = 'p046r JKR++ G3' and '0B42QaQPHLJloNnZhakpiVk9GRmM' in parents").execute()
+
     folders_hash = folders["items"]
 
     for folder in folders_hash:
